15a986cfb41eaa6f1a75dc3f83187cd0e5fda57a
zakyaSharmeen <zakyasharmeen@gmail.com>
2026-05-17 21:48:48 +0530

    cloudinary-set-up

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,10 +1,20 @@
 import express from "express";
 import { isAuth } from "../middlewares/isAuth.js";
 import uploadFiles from "../middlewares/multer.js";
-import { createProduct } from "../controllers/productController.js";
+import {
+  createProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+  updateProductImage,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.post("/product/new", isAuth, uploadFiles, createProduct);
+router.get("/product/all", getAllProducts);
+router.get("/product/:id", getSingleProduct);
+router.put("/product/:id", isAuth, updateProduct);
+router.post("/product/:id", isAuth, uploadFiles, updateProductImage);
 
 export default router;
